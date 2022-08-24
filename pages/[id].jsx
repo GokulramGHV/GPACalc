8b06b655e212e06f880931a40afd3402193a6cd2
Gokulram A <gokulram2010418@ssn.edu.in>
2022-08-24 20:42:01 +0530

    Improved RenderCalc

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { getCalc } from '../utils/api';
 
@@ -13,6 +14,7 @@ const grades = [
 export default function RenderCalc({ data }) {
   const getInitState = () => {
     let initState = [];
+    if (data === 'Error') return;
     data.fields.forEach((field) => {
       initState.push({
         subName: field.subName,
@@ -41,8 +43,27 @@ export default function RenderCalc({ data }) {
     console.log(state);
   }, [state]);
 
+  if (data === 'Error') {
+    return (
+      <div className="flex justify-center items-center min-h-screen flex-col gap-4 bg-slate-50">
+        <h1 className="text-4xl ">
+          <span className="font-bold mr-2">Error 404:</span> Calc Not Found!
+        </h1>
+        <p className="text-lg">Check your Calc URL!</p>
+      </div>
+    );
+  }
+
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       {showGPA ? (
         <div className="h-screen flex flex-col justify-center items-center w-full">
           <h2 className="text-3xl font-medium">Your GPA is</h2>{' '}
@@ -57,22 +78,47 @@ export default function RenderCalc({ data }) {
           </button>
         </div>
       ) : (
-        <div className="h-screen flex justify-center items-center bg-slate-50">
+        <div className="min-h-screen flex justify-center items-center bg-slate-50 px-3 py-14">
           <div className="w-[30rem] rounded-lg p-5 shadow-lg bg-white">
             <h1 className="text-3xl font-bold">{data.title}</h1>
-            <h2 className="text-lg">
+            <h2 className="text-base">
               <span className="font-medium">Template By:</span> {data.createdBy}
             </h2>
-            <div>
+            <hr className="mb-3 mt-2 bg-gray-300" />
+            <div className="grid gap-4">
               {data.fields.map((field, i) => {
                 return (
                   <div key={i}>
-                    {field.subName} {field.credits}
+                    <div className="flex justify-between">
+                      <label htmlFor={`${i}`} className="mb-0.5">
+                        {field.subName}
+                      </label>
+                      <div className="flex mt-0.5 ml-4 text-sm font-medium">
+                        <p className="mr-1">{field.credits}</p>
+                        <p>Credits</p>
+                      </div>
+                    </div>
+
                     <div className="w-full">
                       <select
                         name="grades"
-                        id="grades"
-                        className="w-full"
+                        id={`${i}`}
+                        className="form-control
+                        block
+                        w-full
+                        px-3
+                        py-1.5
+                        text-base
+                        font-normal
+                        text-gray-700
+                        bg-white bg-clip-padding
+                        border-2 border-solid border-gray-300
+                        rounded
+                        transition
+                        ease-in-out
+                        m-0
+                        shadow-sm
+                        focus:text-gray-700 focus:bg-white focus:border-blue-500 focus:shadow-lg focus:shadow-blue-500/20 focus:ring-0 focus:outline-none;"
                         onChange={(e) => {
                           setState((state) =>
                             state.map((f) => {
@@ -114,10 +160,21 @@ export default function RenderCalc({ data }) {
 }
 
 export async function getServerSideProps({ params }) {
-  const data = await getCalc(params.id);
-  return {
-    props: {
-      data,
-    },
-  };
+  let returnVal;
+  try {
+    const data = await getCalc(params.id);
+    returnVal = {
+      props: {
+        data,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    returnVal = {
+      props: {
+        data: 'Error',
+      },
+    };
+  }
+  return returnVal;
 }
